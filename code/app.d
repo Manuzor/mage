21c40168b375a93bbbd0f56fa7629a7474bca96f
Manuel Maier <mjmaier@gmx.de>
2015-05-06 20:28:36 +0200

    Basic dub-powered source structure.

---
+++ code/app.d
@@ -0,0 +1,20 @@
+
+import mage;
+
+int main(string[] args)
+{
+  import std.stdio;
+  import std.getopt;
+
+  string sourceDir;
+  auto helpInfo = getopt(args,
+                         config.required,
+                         "source-dir", "The source dir.", &sourceDir);
+
+  if(helpInfo.helpWanted) {
+    defaultGetoptPrinter("Some info.", helpInfo.options);
+    return 1;
+  }
+
+  return 0;
+}
